179b37d283924baa162fd78e73f0892a02e778bc
alu1006 <andy8050802@gmail.com>
2023-04-08 16:36:17 +0800

    多個檔案壓縮修復

--- api/app.py
+++ api/app.py
@@ -1,12 +1,12 @@
 import os
 import zipfile
 from datetime import date
-from io import BytesIO
+from io import BytesIO, StringIO
 
 from flask import Flask, request, send_file, render_template
 import opencc
 import docx2txt
-from io import BytesIO, StringIO
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,39 +15,45 @@ def index():
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    file = request.files['files']
+    files = request.files.getlist('files')
+    converted_files = {}
 
-    if file.filename.endswith('.docx'):
-        text = docx2txt.process(file)
-    else:
-        text = file.read().decode('utf-8', 'ignore')
+    for file in files:
+        if file.filename.endswith('.docx'):
+            text = docx2txt.process(file)
+        else:
+            text = file.read().decode('utf-8', 'ignore')
 
-    converter = opencc.OpenCC('s2t')
-    text = converter.convert(text)
+        converter = opencc.OpenCC('s2t')
+        text = converter.convert(text)
 
-    filename = os.path.splitext(file.filename)[0] + '_convert.txt'
+        filename = os.path.splitext(file.filename)[0] + '_convert.txt'
 
-    text_buffer = StringIO()
-    text_buffer.write(text)
-    text_buffer.seek(0)
-    # Convert StringIO to BytesIO
-    bytes_buffer = BytesIO(text_buffer.getvalue().encode('utf-8'))
+        text_buffer = StringIO()
+        text_buffer.write(text)
+        text_buffer.seek(0)
+        # Convert StringIO to BytesIO
+        bytes_buffer = BytesIO(text_buffer.getvalue().encode('utf-8'))
 
+        # Save the file to the in-memory dictionary
+        converted_files[filename] = bytes_buffer.getvalue()
 
-    if request.form.get('multiple'):
+    if request.form.get('multiple') or len(converted_files) > 1:
         zip_filename = f'{date.today().strftime("%Y%m%d")}_convert.zip'
 
         zip_buffer = BytesIO()
         with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
-            for filename in os.listdir():
-                if filename.endswith('_convert.txt'):
-                    zip_file.write(filename)
+            for filename, file_content in converted_files.items():
+                # Add the file to the zip archive using a BytesIO object
+                zip_file.writestr(filename, file_content)
 
         zip_buffer.seek(0)
-        return send_file(zip_buffer, attachment_filename=zip_filename, as_attachment=True)
+        return send_file(zip_buffer, download_name=zip_filename, as_attachment=True, mimetype='application/zip')
 
     else:
-        return send_file(bytes_buffer, download_name=filename, as_attachment=True,mimetype='text/plain')
+        filename, file_content = list(converted_files.items())[0]
+        bytes_buffer = BytesIO(file_content)
+        return send_file(bytes_buffer, download_name=filename, as_attachment=True, mimetype='text/plain')
 
     return render_template('index.html')
 
